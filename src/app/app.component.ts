d43ad537391bc34148ebe6bb30fdd6239259ba6b
Darren <darren@darrenmothersele.com>
2020-01-28 09:42:10 +0000

    add link back to github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <div class="max-w-sm container mx-auto mt-6">
-        <router-outlet></router-outlet>
+    <div class="max-w-sm mx-auto">
+        <div class="bg-gray-200 mb-4">
+            <p class="text-center text-xs">View on <a class="text-blue-700 font-medium" href="https://github.com/darrenmothersele/swipe-button">GitHub</a></p>
+        </div>
+        <div class="mt-6">
+            <router-outlet></router-outlet>
+        </div>
     </div>
   `,
   styles: []
